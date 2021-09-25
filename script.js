133b722c4edc52cccbe3b8ca95bb17737efba8c7
saud alkhazriji <saud11alkh@gmail.com>
2021-09-25 17:59:13 +0300

    files

--- script.js
+++ script.js
@@ -25,9 +25,54 @@ document.getElementById('drop-down').addEventListener('click', function() {
     if (nav.classList.contains('side-bar')) {
         nav.classList.remove('side-bar');
         nav.classList.add('side-bar_active');
-        console.log('drop')
     } else {
         nav.classList.remove('side-bar_active');
         nav.classList.add('side-bar');
     }
 })
+
+
+//i gave all the prices the same id which will not work so i need to figure out another way
+
+//maybe i can get the meal && burger class store it in an array check which index was  
+//clicked then check the price array, and when they match apply the change
+
+const priceEl = document.getElementsByClassName('price');
+const buttonsEl = document.getElementsByClassName('meal-burger-btn');
+
+function burgerPrice() {
+    document.getElementBy('price').textContent = '1.8 bd';
+}
+
+function mealPrice() {
+    document.getElementById('price').textContent = '2.3 bd';
+}
+
+priceEl[0].id = 'hello-my-man';
+console.log(priceEl[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function burgerPrice() {
+    document.getElementBy('price').textContent = '1.8 bd';
+}
+
+function mealPrice() {
+    document.getElementById('price').textContent = '2.3 bd';
+}
+*/
